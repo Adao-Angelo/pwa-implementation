fd3757e6c441ef4a59adad39f3443ec182828cb5
Adao-Angelo <adaobegginer@gmail.com>
2024-07-08 11:52:49 +0100

    Init project

---
+++ scripts.js
@@ -0,0 +1,19 @@
+function handleFileSelect() {
+  const input = document.getElementById("imageUpload");
+  const uploadButton = document.getElementById("uploadButton");
+
+  if (input.files && input.files[0]) {
+    uploadButton.style.display = "block";
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      document.getElementById(
+        "imagePreview"
+      ).innerHTML = `<img src="${e.target.result}" alt="Image Preview">`;
+    };
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
+function uploadImage() {
+  alert("Image uploaded successfully!");
+}
